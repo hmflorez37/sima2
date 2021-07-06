8aed4ebb3308a7017dd6beee62f5081f92fa8fef
hmflorez37 <67834648+hmflorez37@users.noreply.github.com>
2021-07-06 14:28:52 -0500

    Delete sima2 directory

--- sima2/controlador/listar_clientes.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $sentencia=$pdo->prepare("SELECT * FROM cliente");
-    $sentencia->execute();
-    $listaclientes=$sentencia->fetchAll(PDO::FETCH_ASSOC);
-
--- sima2/controlador/listar_ventatemp.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $sentencia=$pdo->prepare("SELECT * FROM temp");
-    $sentencia->execute();
-    $prodlistado=$sentencia->fetchAll(PDO::FETCH_ASSOC);
-
