68e7c143443ccde8885589f57f0ac54f5acaabd6
hmflorez37 <67834648+hmflorez37@users.noreply.github.com>
2021-08-03 14:05:09 -0500

    presentacion

--- sima2/index.php
+++ sima2/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="google-signin-client_id" content="191497136417-4cjnsdfdju6kbllci4fukqdhpoucbfkj.apps.googleusercontent.com">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +10,7 @@
     <link rel="stylesheet" href="./CSS/login-min.css">
 
     <!--scripts-->
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <script src="assets/js/signin.js"></script>
-    <script src="JS/jquery-3.6.0.min.js"></script>
+
 
 
     <title>S.I.M.A</title>
@@ -26,18 +24,18 @@
             </div>
             <div class="col bg-white p-5 rounded-end usuario">
                 <div class="text-end">
-                    <img src="./IMAGES/sima logo.jpeg" width="50"alt="">
+                    <img src="./IMAGES/sga.png" width="50"alt="">
                 </div>
-                <h2 class="fw-bold text-center pt-5 py-5">Bienvenido a <br>S.G.A</h2>
+                <h2 id="l"class="fw-bold text-center pt-5 py-5">Bienvenido a <br>S.G.A</h2>
 
                 <!--login-->
                 <form action="controlador/sesion.php" method="post">
                     <div class="mb-4">
-                        <label for="user-name" class="
+                        <label id="l"for="user-name" class="
                         form-label">nombre de usuario</label>
                         <input type="text" class="form-control" name="user-name"required>
                     </div>
-                    <div class="mb-4"> <label for="password" class="
+                    <div class="mb-4"> <label id="l"for="password" class="
                         form-label">password</label>
                         <input type="password" class="form-control" name="password"required></div>
                     <div class="d-grid mb-4">
